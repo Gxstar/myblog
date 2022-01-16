3333e35f2457cc32a6762024ad1a7c25e5f4fb24
Gxstar <gengxing123@qq.com>
2022-01-16 23:13:22 +0800

    构建组建

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@fortawesome/fontawesome-free/js/all.min.js'
 
 createApp(App).use(router).mount('#app')
