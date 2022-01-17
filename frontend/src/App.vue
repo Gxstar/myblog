b9704d71c4791b72d566880ecb1b3cdc43616a0a
gxstar <gengxing123@qq.com>
2022-01-17 17:39:26 +0800

    细节改动

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="container-md">
+    <button type="button" class="btn btn-primary">Primary</button>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -17,16 +18,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
