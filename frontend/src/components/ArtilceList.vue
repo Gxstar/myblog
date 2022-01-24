ed59c80219fc310d3ba97ade2da1e580ea7a4053
gxstar <gengxing123@qq.com>
2022-01-24 18:53:53 +0800

    文章列表独立为插件

---
+++ frontend/src/components/ArtilceList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="article-list">
+    <h6>最新文章</h6>
+    <section>
+      <article v-for="post in posts" :key="post.id" class="row list-body">
+        <div class="col-12 col-md-5" v-if="post.hasCover">
+          <img
+            :src="post.cover"
+            alt=""
+            loading="lazy"
+            class="img-fluid img-thumbnail"
+          />
+        </div>
+        <div class="col-12 col-md-7">
+          <header>{{ post.title }}</header>
+          <p>{{ post.intro }}</p>
+        </div>
+      </article>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArtcleList",
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="home">
-    <h6>最新文章</h6>
-    <section>
-      <article v-for="post in posts" :key="post.id" class="row list-body">
-        <div class="col-5" v-if="post.hasCover">
-          <img :src="post.cover" alt="" srcset="" style="width: 100%" />
-        </div>
-        <div class="col-7">
-          <header>{{ post.title }}</header>
-          <p>{{ post.intro }}</p>
-        </div>
-      </article>
-    </section>
+    
   </div>
 </template>
 
@@ -29,7 +18,7 @@ export default {
           id: 1,
           title: "第一篇文章",
           cover:
-            "https://gxstar123.cn/wp-content/uploads/2022/01/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20220121225908-769x469.jpg",
+            "https://s1.ax1x.com/2020/10/06/0UwokV.md.jpg",
           intro: "这是第一篇文章的简介",
           text: "这是第一篇文章的简介,大沙发斯蒂芬卡世纪东方卡视角东方红卡闪电发货尅五花肉饭度搜阿福卡拉时代峻峰虎课网哈人防守打法可好看",
           hasCover: true,
@@ -38,7 +27,7 @@ export default {
           id: 2,
           title: "第二篇文章",
           cover:
-            "https://gxstar123.cn/wp-content/uploads/2022/01/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20220121225908-769x469.jpg",
+            "https://s1.ax1x.com/2020/10/06/0Uw5T0.md.jpg",
           intro: "这是第二篇文章的简介",
           text: "这是第二篇文章的简介,大沙发斯蒂芬卡世纪东方卡视角东方红卡闪电发货尅五花肉饭度搜阿福卡拉时代峻峰虎课网哈人防守打法可好看",
           hasCover: false,
